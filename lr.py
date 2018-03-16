c0f61c33a242539032cf6a87503c7d63d15e4134
morfast <morfast@example.com>
2018-03-16 17:50:32 +0800

    OK

--- lr.py
+++ lr.py
@@ -15,6 +15,7 @@ step 5: update the weights and bias
 
 import numpy as np
 from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
 
 class LR:
     
@@ -29,17 +30,15 @@ class LR:
 
     def cross_entropy(self, y_truth, y_predict):
         """ cross entropy """
-        print y_truth
-        print y_predict
-        s = np.sum(y_truth * np.log(y_predict) + (1.0 - y_truth) * (1.0 - np.log(y_predict)))
+        s = np.sum(y_truth * np.log(y_predict) + (1.0 - y_truth) * (np.log(1 - y_predict)))
         return (-1.0 * s)/ y_truth.size
 
     def train(self, x, y_truth, max_iter=100, learning_rate=0.05):
         # initialize the parameters
         n_sample, n_feature = x.shape
         print "n_sample:", n_sample, "n_feature", n_feature
-        self._weights_ = np.zeros(n_feature)  # vector
-        self._bias_ = 0.0  # scalar
+        self._weights_ = np.random.rand(n_feature)/100  # vector
+        self._bias_ = np.random.rand()/100  # scalar
 
         for n_iter in range(max_iter):
             # compute the linear combination, and apply the sigmoid function
@@ -47,19 +46,23 @@ class LR:
 
             # compute the loss
             loss = self.cross_entropy(y_truth, y_predict)
-            print "iter: %d, loss: %f" % (n_iter, loss)
+            print "iter: %d, loss: %f weight %f bias %f" % \
+                 (n_iter, loss, np.sum(self._weights_), np.sum(self._bias_)),
 
             if np.abs(loss) < 1.0e-4:
                 print "Converged"
                 break
 
             # compute the gradient
-            dw = (1 / n_sample) * np.dot(x.T, (y_predict - y_truth))
-            db = (1 / n_sample) * np.sum(y_predict - y_truth)
+            dw = (1.0 / n_sample) * np.dot(x.T, (y_predict - y_truth))
+            db = (1.0 / n_sample) * np.sum(y_predict - y_truth)
+            print "dw", np.sum(np.abs(dw)), "db", np.sum(np.abs(db))
 
             # update the parameters
             self._weights_ -= learning_rate * dw
             self._bias_ -= learning_rate * db
+
+        self._trained_ = True
 
 
     def predict(self, x, threshold = 0.50):
@@ -67,15 +70,20 @@ class LR:
             print "Not trained"
             return
 
-        y_predict = self.sigmoid(np.dot(self._weights_, x) + self._bias_)
+        y_predict = self.sigmoid(np.dot(x, self._weights_) + self._bias_)
         y_predict_labels = [ 1 if elem > threshold else 0 for elem in y_predict]
 
         return np.array(y_predict_labels)
 
 
 def test():
-    x, y_true = make_blobs(n_samples=10, centers=2)
+    x, y_true = make_blobs(n_samples=300, centers=2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y_true)
+
     model = LR()
-    model.train(x, y_true)
+    model.train(x_train, y_train, max_iter=100)
+
+    y_p = model.predict(x_test)
+    print "error:", np.mean(np.abs(y_test - y_p))
 
 test()
